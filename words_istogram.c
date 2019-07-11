0844d5aada4347d864150c2ceed4233de480129f
Guidi Simone <gsimone21892@gmail.com>
2019-07-11 12:05:42 +0200

    finished program for making a histogram of the words

--- words_istogram.c
+++ words_istogram.c
@@ -14,11 +14,9 @@ easy to draw the histogram with the bars horizontal; a vertical orientation is m
 int main()
 {
     int histogram[10]; //histogram 0-10 are words of lenght 0-10
-    int c;
+    int c, in_words, word_length, max_lenght, words_num;
 
-    int in_words = 1;
-    int word_length = 0;
-    int max_lenght = 0;
+    in_words = word_length = max_lenght = words_num = 0;
 
     for(int j = 0; j<10; ++j)
     {
@@ -27,25 +25,44 @@ int main()
 
     while((c = getchar()) != EOF)
     {
-        if(c != ' ' || c != '\t' || c != '\n')
+        // printf("---------------------------\n");
+        // printf("c:%c\n", c);
+        // printf("in_words:%d\n", in_words);
+        // printf("word_length:%d\n", word_length);
+        
+        if(c == ' ' || c == '\t' || c == '\n') // i have reached a special char, the word end
         {
-            ++word_length;
-            if(word_length > max_lenght && word_length <= 10)
-            {
-                max_lenght = word_length;
-            }
-        }        
-        else
-        {
-            //Out of words
-            if(word_length < 10)
+            if(in_words == 1)
             {
                 ++histogram[word_length];
-            }            
+                if(histogram[word_length] > max_lenght && histogram[word_length] < 10)
+                {
+                    max_lenght = word_length;
+                }
+                word_length = 0;
+                in_words = 0;
+            }
+        }    
+        else // c is a char
+        {
+            // printf("normal char\n");
+            if(in_words == 0)
+            {
+                // printf("next word\n");
+                ++words_num;
+                in_words = 1;
+            }
+            ++word_length;
         }
     }
+    if(in_words == 1)
+    {
+        ++histogram[word_length];
+        word_length = 0;
+        in_words = 0;
+    }
 
-    for(int i=0; i< 10; ++i)
+    for(int i=1; i< 10; ++i)
     {
         //Print histogram bars
         printf("%d ", i); // Number
@@ -53,12 +70,13 @@ int main()
         {
             printf("#");
         }
-        //End line
         printf("\n");
     }
-    for(int j = 0; j<max_lenght; ++j)
+    printf("x ");
+    for(int j = 1; j < max_lenght; ++j)
     {
         printf("%d", j);
     }
     printf("\n");
+    printf("w:%d\n", words_num);
 }
